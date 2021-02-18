b35102c9ddd01679a660691e0ad69c0e5bc19867
Eplundare <aelozanoa@gmail.com>
2021-02-18 17:17:03 +0100

    Added code on VideoPlayerManager and StudentMedia to change ratio of videos which need it.

--- Assets/Scripts/StudentMedia.cs
+++ Assets/Scripts/StudentMedia.cs
@@ -17,7 +17,24 @@ public class StudentMedia : MonoBehaviour
     [Header("is vertical, check 'isVertical':", order = 4)]
 
     public bool isVertical;
+
+    //[Space]
+    //[Header("If the presentation video is horizontal, uncheck 'isVertical'. If the pres. video is vertical, check 'isVertical':")]
+    [Header("These bools only work if the,", order = 0)]
+    [Space(-10, order = 1)]
+    [Header("video IS NOT VERTICAL.", order = 2)]
+    [Space(-10, order = 3)]
+    [Header("Also, if the presentation video has", order = 4)]
+    [Space(-10, order = 5)]
+    [Header("a ratio DIFFERENT FROM 16:9,", order = 6)]
+    [Space(-10, order = 7)]
+    [Header("only check the right bool:", order = 8)]
+    public bool presentationIs1x1Square;
+    public bool presentationIs4x3ShortRectangl;
+    public bool presentationIs21x9LongRectangl;
+
     public YoutubePlayer horizontalMediaYtPlayer;
+    public Transform horizontalVideoRenderArea;
     public YoutubePlayer verticalMediaYtPlayer;
     public string presentationUrl;
     public string artifactUrl;
