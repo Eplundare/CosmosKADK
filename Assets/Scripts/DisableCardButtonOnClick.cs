179010cef744717082db54e39a683c6a34cf5d5f
Eplundare <aelozanoa@gmail.com>
2021-02-18 22:11:03 +0100

    Started implementing Card Button and Deck Animations.

---
+++ Assets/Scripts/DisableCardButtonOnClick.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableCardButtonOnClick : MonoBehaviour
+{
+    public Button butt;
+    public Image buttImage;
+
+    public bool enablesOtherButt;
+    public Button otherButt;
+
+    // Start is called before the first frame update
+    public void DisableButton()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            butt.interactable = false;
+            buttImage.raycastTarget = false;
+
+            if (enablesOtherButt == true)
+            {
+                otherButt.interactable = true;
+            }
+        }
+    }
+
+    public void EnableButton()
+    {
+        butt.interactable = true;
+        buttImage.raycastTarget = true;
+
+    }
+}
